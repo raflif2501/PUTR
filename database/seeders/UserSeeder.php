87d08a41be31dae2ad0f154bfb3c8651d56cde12
AndyYulianto11 <andyyulianto40@gmail.com>
2022-07-24 00:56:52 +0700

    abc

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -17,12 +17,19 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-         Role::create([
-         'name' => 'admin',
-         ]);
-         Role::create([
-         'name' => 'user',
-         ]);
+        Role::create([
+            'name' => 'admin',
+        ]);
+        Role::create([
+            'name' => 'ppko',
+        ]);
+        Role::create([
+            'name' => 'pptk',
+        ]);
+        Role::create([
+            'name' => 'verificator',
+        ]);
+
 
         // Administrator
          $admin = User::create([
@@ -45,43 +52,43 @@ class UserSeeder extends Seeder
          'email' => 'sekdis@ppko.com',
          'password' => bcrypt('PPKoSekdis;'),
          ]);
-         $user->assignRole('user');
+         $user->assignRole('ppko');
          $user = User::create([
          'name' => 'Sumber Daya Air PPKo',
          'email' => 'sumberdayaair@ppko.com',
          'password' => bcrypt('PPKoSDA;'),
          ]);
-         $user->assignRole('user');
+         $user->assignRole('ppko');
          $user = User::create([
          'name' => 'Bina Marga PPKo',
          'email' => 'binamarga@ppko.com',
          'password' => bcrypt('PPKoBMtop;'),
          ]);
-         $user->assignRole('user');
+         $user->assignRole('ppko');
          $user = User::create([
          'name' => 'Penataan Bangunan dan Gedung PPKo',
          'email' => 'penataanbangundandangedung@ppko.com',
          'password' => bcrypt('PPKoPBG;'),
          ]);
-         $user->assignRole('user');
+         $user->assignRole('ppko');
          $user = User::create([
          'name' => 'Air Minum dan PLP PPKo',
          'email' => 'airminumplp@ppko.com',
          'password' => bcrypt('PPKoAMPLP;'),
          ]);
-         $user->assignRole('user');
+         $user->assignRole('ppko');
          $user = User::create([
          'name' => 'Bina Jasa Konstruksi PPKo',
          'email' => 'binajasakonstruksi@ppko.com',
          'password' => bcrypt('PPKoBJK;'),
          ]);
-         $user->assignRole('user');
+         $user->assignRole('ppko');
          $user = User::create([
          'name' => 'Tata Ruang PPKo',
          'email' => 'tataruang@ppko.com',
          'password' => bcrypt('PPKoTRtop;'),
          ]);
-         $user->assignRole('user');
+         $user->assignRole('ppko');
 
 
         // PPTK
@@ -90,43 +97,43 @@ class UserSeeder extends Seeder
          'email' => 'sekdis@pptk.com',
          'password' => bcrypt('PPTKSekdis;'),
          ]);
-         $user->assignRole('user');
+         $user->assignRole('pptk');
          $user = User::create([
          'name' => 'Sumber Daya Air PPTK',
          'email' => 'sumberdayaair@pptk.com',
          'password' => bcrypt('PPTKSDA;'),
          ]);
-         $user->assignRole('user');
+         $user->assignRole('pptk');
          $user = User::create([
          'name' => 'Bina Marga PPTK',
          'email' => 'binamarga@pptk.com',
          'password' => bcrypt('PPTKBMtop;'),
          ]);
-         $user->assignRole('user');
+         $user->assignRole('pptk');
          $user = User::create([
          'name' => 'Penataan Bangunan dan Gedung PPTK',
          'email' => 'penataanbangundandangedung@pptk.com',
          'password' => bcrypt('PPTKPBG;'),
          ]);
-         $user->assignRole('user');
+         $user->assignRole('pptk');
          $user = User::create([
          'name' => 'Air Minum dan PLP PPTK',
          'email' => 'airminumplp@pptk.com',
          'password' => bcrypt('PPTKAMPLP;'),
          ]);
-         $user->assignRole('user');
+         $user->assignRole('pptk');
          $user = User::create([
          'name' => 'Bina Jasa Konstruksi PPTK',
          'email' => 'binajasakonstruksi@pptk.com',
          'password' => bcrypt('PPTKBJK;'),
          ]);
-         $user->assignRole('user');
+         $user->assignRole('pptk');
          $user = User::create([
          'name' => 'Tata Ruang PPTK',
          'email' => 'tataruang@pptk.com',
          'password' => bcrypt('PPTKTRtop;'),
          ]);
-         $user->assignRole('user');
+         $user->assignRole('pptk');
 
 
         //  Verifikator
@@ -135,48 +142,48 @@ class UserSeeder extends Seeder
         'email' => 'verifikator1@verifikator.com',
         'password' => bcrypt('V3r1f1k4t0r;'),
         ]);
-        $user->assignRole('user');
+        $user->assignRole('verificator');
         $user = User::create([
         'name' => 'Verifikator 2',
         'email' => 'verifikator2@verifikator.com',
         'password' => bcrypt('V3r1f1k4t0r;'),
         ]);
-        $user->assignRole('user');
+        $user->assignRole('verificator');
         $user = User::create([
         'name' => 'Verifikator 3',
         'email' => 'verifikator3@verifikator.com',
         'password' => bcrypt('V3r1f1k4t0r;'),
         ]);
-        $user->assignRole('user');
+        $user->assignRole('verificator');
         $user = User::create([
         'name' => 'Verifikator 4',
         'email' => 'verifikator4@verifikator.com',
         'password' => bcrypt('V3r1f1k4t0r;'),
         ]);
-        $user->assignRole('user');
+        $user->assignRole('verificator');
         $user = User::create([
         'name' => 'Verifikator 5',
         'email' => 'verifikator5@verifikator.com',
         'password' => bcrypt('V3r1f1k4t0r;'),
         ]);
-        $user->assignRole('user');
+        $user->assignRole('verificator');
         $user = User::create([
         'name' => 'Verifikator 6',
         'email' => 'verifikator6@verifikator.com',
         'password' => bcrypt('V3r1f1k4t0r;'),
         ]);
-        $user->assignRole('user');
+        $user->assignRole('verificator');
         $user = User::create([
         'name' => 'Verifikator 7',
         'email' => 'verifikator7@verifikator.com',
         'password' => bcrypt('V3r1f1k4t0r;'),
         ]);
-        $user->assignRole('user');
+        $user->assignRole('verificator');
         $user = User::create([
         'name' => 'Verifikator 8',
         'email' => 'verifikator8@verifikator.com',
         'password' => bcrypt('V3r1f1k4t0r;'),
         ]);
-        $user->assignRole('user');
+        $user->assignRole('verificator');
     }
 }
