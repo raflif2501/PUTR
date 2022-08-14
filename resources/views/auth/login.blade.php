eeb4a6c494680654744a21a421bdd2f9c9e96a6c
RAFFF <raflifirdausfalaka@gmail.com>
2022-08-14 21:01:00 +0700

    login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -45,7 +45,7 @@
                                 </div>
                             </div>
                             <div class="form-group">
-                                <button type="submit" class="btn btn-primary submit px-3">Login</button>
+                                <button type="submit" class="form-control btn btn-primary submit px-3">Login</button>
                             </div>
                         </form>
                     </div>
